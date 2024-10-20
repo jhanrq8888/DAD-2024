ac3d32465ae41ea31a323cf02818735c08dfa551
jhanrq8888 <jhanramos8888@gmail.com>
2024-10-19 21:43:23 -0500

    solucion de errores

--- ms-pedido/src/main/java/com/example/mspedido/feign/ClienteFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/ClienteFeign.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-cliente-service", path = "/clientes")
+@FeignClient(name = "ms-cliente-service", path = "/cliente")
 public interface ClienteFeign {
 
     @GetMapping("/{id}")
