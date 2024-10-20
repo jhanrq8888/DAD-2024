f313448afbbb09fbc79fec1c1218493f507572ea
jhanrq8888 <jhanramos8888@gmail.com>
2024-10-19 21:10:17 -0500

    solucion de errores

--- ms-envio/src/main/java/com/example/msenvio/feign/ProductoFeign.java
+++ ms-envio/src/main/java/com/example/msenvio/feign/ProductoFeign.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity; // Importa la clase ResponseEnti
 import org.springframework.web.bind.annotation.GetMapping; // Importa la anotación para solicitudes GET
 import org.springframework.web.bind.annotation.PathVariable; // Importa la anotación para variables de ruta
 
-@FeignClient(name = "ms-Producto-service", path = "/producto") // Define el cliente Feign para comunicarse con el microservicio de producto
+@FeignClient(name = "ms-Producto-service", path = "/product") // Define el cliente Feign para comunicarse con el microservicio de producto
 public interface ProductoFeign {
 
     @GetMapping("/{id}") // Mapea la solicitud GET a "/producto/{id}"
