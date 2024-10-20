f984b374356a1413470e40705751e65f2539ed03
jhanrq8888 <jhanramos8888@gmail.com>
2024-10-19 20:29:41 -0500

    solucion de errores

--- ms-pedido/src/main/java/com/example/mspedido/entity/Pedido.java
+++ ms-pedido/src/main/java/com/example/mspedido/entity/Pedido.java
@@ -20,7 +20,7 @@ public class Pedido {
     private Integer clienteId;
     @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "pedido_id")
+    @JoinColumn(name = "venta_id")
     private List<PedidoDetalle> pedidodetalle;
 
     @Transient
